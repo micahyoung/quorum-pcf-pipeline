f676d3628e0aa600522f5eb255f8992ea98d9c99
Micah Young <myoung@pivotal.io>
2017-05-09 23:33:31 -0400

    Improve task

--- tasks/build-task.sh
+++ tasks/build-task.sh
@@ -4,8 +4,12 @@ set -o nounset
 set -o pipefail
 
 SRC_DIR=${SRC_DIR:?"!"}
+PIPELINE_DIR=${PIPELINE_DIR:?"!"}
 OUTPUT_DIR=${OUTPUT_DIR:?"!"}
 
 pushd $SRC_DIR
   make geth
 popd
+
+cp $SRC_DIR/build/bin/geth $OUTPUT_DIR/geth
+cp $PIPELINE_DIR/manifest.yml $OUTPUT_DIR/manifest.yml
